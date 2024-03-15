e8b91f0bf1dcd9284e69bfb6016dc3a9296a3d61
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-03-15 20:34:18 +0200

    chore

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="flex min-h-screen flex-col">
+    <div
+        class="flex min-h-screen flex-col bg-stone-100 font-dm dark:bg-slate-900 dark:text-stone-100"
+    >
         <Header />
         <div class="flex-auto">
             <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,28 @@
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: [
+        "@nuxt/image",
         "@nuxtjs/eslint-module",
         "@nuxtjs/tailwindcss",
-        "@nuxt/image",
+        "@nuxtjs/color-mode",
         "@nuxtjs/google-fonts",
+        "nuxt-headlessui",
+        "nuxt-icon"
     ],
     typescript: {
         typeCheck: true,
     },
+    colorMode: {
+        classSuffix: ''
+    },
     tailwindcss: {
-        exposeConfig: true,
+        exposeConfig: true
     },
     googleFonts: {
         families: {
             Roboto: true,
             Lato: true,
+            'DM Sans': true
         },
         download: true,
         display: "swap",
