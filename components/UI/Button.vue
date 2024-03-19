7de94c49f1136c5cf4790fcf79166b82e0d7068f
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-03-19 20:26:33 +0200

    chore: UI button

---
+++ components/UI/Button.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="rounded-md bg-zinc-900 text-white shadow-md transition hover:bg-themePurple active:scale-95 dark:bg-themeGreen dark:text-black dark:hover:bg-themeBlue"
+    >
+        <slot />
+    </button>
+</template>
