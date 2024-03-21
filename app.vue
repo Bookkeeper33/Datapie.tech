3764efc0fc8b88a91726013cbf81d838b0bbcfbb
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-03-21 17:22:21 +0200

    clean up

--- app.vue
+++ app.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="flex min-h-screen flex-col font-dm dark:text-stone-100">
         <Header />
-        <main class="flex-auto">
-            <NuxtPage />
-        </main>
+        <NuxtPage />
         <Footer />
     </div>
 </template>
