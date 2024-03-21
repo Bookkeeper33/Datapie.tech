610319bf9386e7e780c7c8b6f2b861e08effe4e7
Bookkeeper <yarko12@gmail.com>
2024-03-21 19:03:24 +0200

    feature: services section

---
+++ components/MainPage/Services.vue
@@ -0,0 +1,9 @@
+<template>
+    <section id="services">
+        <div class="container mx-auto min-h-screen">
+            <h2 class="ml-20 text-5xl font-bold">What we do</h2>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup></script>
