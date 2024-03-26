bb1de776dd92dd9cbabdde346b9774266fba03d0
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-03-26 21:03:59 +0200

    clean up

--- components/MainPage/Services.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <section id="services">
-        <div class="container mx-auto min-h-screen">
-            <h2 class="ml-20 text-5xl font-bold">What we do</h2>
-        </div>
-    </section>
-</template>
-
-<script lang="ts" setup></script>
