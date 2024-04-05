4bf40b0dc3adddc3c21a4bb968487d8918e00bce
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-04-05 19:15:15 +0300

    fix: resolve hydration mismatch

--- components/MainPage/Benefits/Card.vue
+++ components/MainPage/Benefits/Card.vue
@@ -1,24 +1,21 @@
 <script lang="ts" setup>
     defineProps<{ benefit: Content }>();
-
+    
     const card = ref<HTMLElement | null>(null);
-    const { x, y } = useMouse({ target: card, type: "client", touch: false });
+    const { x, y } = useMouse({
+        target: card,
+        type: "client",
+        touch: false,
+    });
     const { left, top } = useElementBounding(card);
-    const { width } = useWindowSize();
-    const MIN_SCREEN_WIDTH = 1024;
 
     const glowX = computed(() => x.value - left.value);
     const glowY = computed(() => y.value - top.value);
-
     const cardStyle = computed(() => {
-        if (width.value < MIN_SCREEN_WIDTH) {
-            return;
-        } else {
-            return {
-                "--glow-x": `${glowX.value}px`,
-                "--glow-y": `${glowY.value}px`,
-            };
-        }
+        return {
+            "--glow-x": `${glowX.value}px`,
+            "--glow-y": `${glowY.value}px`,
+        };
     });
 </script>
 
@@ -29,13 +26,13 @@
         :style="cardStyle"
     >
         <div
-            class="card-body z-0 rounded-[inherit] bg-themeGreen/50 *:opacity-80 *:transition *:duration-300 *:ease-in-out group-hover:*:opacity-100 dark:bg-themePurple/80"
+            class="card-body z-0 rounded-[inherit] bg-themeGreen/50 *:opacity-80 *:transition *:duration-300 *:ease-in-out dark:bg-themePurple/80 group-hover:lg:*:opacity-100"
         >
             <Icon :name="benefit.img" size="3em" />
             <h2 class="card-title">
                 {{ benefit.title }}
             </h2>
-            <p>
+            <p class="text-base">
                 {{ benefit.description }}
             </p>
         </div>
