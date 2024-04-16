90a77e12c6601b383d679a9dde74bbf705174ff4
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-04-16 14:24:05 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-    content: [
-        "./components/**/*.{js,vue,ts}",
-        "./layouts/**/*.vue",
-        "./pages/**/*.vue",
-        "./plugins/**/*.{js,ts}",
-        "./app.vue",
-        "./error.vue",
-      ],
+    content: [],
     darkMode: 'class',
     theme: {
         extend: {
@@ -26,53 +19,11 @@ export default {
                 'radial-dark':'radial-gradient(1500px circle at var(--glow-x) var(--glow-y),rgba(80, 246, 158, 0.9), transparent 18%)',
                 'radial': 'radial-gradient(1500px circle at var(--glow-x) var(--glow-y), rgba(31, 115, 233, 0.9), transparent 18%)',
             },
-            typography: ({ theme }) => ({
-                slate: {
-                  css: {
-                    '--tw-prose-body': theme('colors.slate[800]'),
-                    '--tw-prose-headings': theme('colors.slate[900]'),
-                    '--tw-prose-lead': theme('colors.slate[700]'),
-                    '--tw-prose-links': theme('colors.slate[900]'),
-                    '--tw-prose-bold': theme('colors.slate[900]'),
-                    '--tw-prose-counters': theme('colors.slate[600]'),
-                    '--tw-prose-bullets': theme('colors.slate[400]'),
-                    '--tw-prose-hr': theme('colors.slate[300]'),
-                    '--tw-prose-quotes': theme('colors.slate[900]'),
-                    '--tw-prose-quote-borders': theme('colors.slate[300]'),
-                    '--tw-prose-captions': theme('colors.slate[700]'),
-                    '--tw-prose-code': theme('colors.slate[900]'),
-                    '--tw-prose-pre-code': theme('colors.slate[100]'),
-                    '--tw-prose-pre-bg': theme('colors.slate[900]'),
-                    '--tw-prose-th-borders': theme('colors.slate[300]'),
-                    '--tw-prose-td-borders': theme('colors.slate[200]'),
-                    '--tw-prose-invert-body': theme('colors.slate[200]'),
-                    '--tw-prose-invert-headings': theme('colors.white'),
-                    '--tw-prose-invert-lead': theme('colors.slate[300]'),
-                    '--tw-prose-invert-links': theme('colors.white'),
-                    '--tw-prose-invert-bold': theme('colors.white'),
-                    '--tw-prose-invert-counters': theme('colors.slate[400]'),
-                    '--tw-prose-invert-bullets': theme('colors.slate[600]'),
-                    '--tw-prose-invert-hr': theme('colors.slate[700]'),
-                    '--tw-prose-invert-quotes': theme('colors.slate[100]'),
-                    '--tw-prose-invert-quote-borders': theme('colors.slate[700]'),
-                    '--tw-prose-invert-captions': theme('colors.slate[400]'),
-                    '--tw-prose-invert-code': theme('colors.white'),
-                    '--tw-prose-invert-pre-code': theme('colors.slate[300]'),
-                    '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
-                    '--tw-prose-invert-th-borders': theme('colors.slate[600]'),
-                    '--tw-prose-invert-td-borders': theme('colors.slate[700]'),
-                  },
-                },
-              }),
-        },
     },
     plugins: [require('daisyui'),  require('@tailwindcss/typography'),],
     daisyui: {
       themes: [],
-      
-      base: false, // applies background color and foreground color for root element by default
-      styled: true, // include daisyUI colors and design decisions for all components
-      utils: true, // adds responsive and modifier utility classes
-      themeRoot: ":root", // The element that receives theme color CSS variables
+      base: false,
     },
+  },
 };
