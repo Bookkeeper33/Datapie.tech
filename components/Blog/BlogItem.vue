92b8be057f01e871fe3b81540146fc1b4a8f6909
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-04-16 17:37:21 +0300

    feat: - separate UI for section introduction - List of blogs (no route)

---
+++ components/Blog/BlogItem.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+    defineProps<{ post: BlogPost }>();
+</script>
+
+<template>
+    <div
+        class="group cursor-pointer rounded-xl bg-gradient-to-r from-themeGreen via-themeBlue to-themePurple p-0.5 shadow-xl transition lg:relative lg:z-[1] lg:before:absolute lg:before:bottom-0 lg:before:left-0 lg:before:right-0 lg:before:top-0 lg:before:-z-[1] lg:before:rounded-xl lg:before:bg-gradient-to-b lg:before:opacity-0 lg:before:transition-opacity lg:before:duration-500 lg:before:ease-linear lg:hover:shadow-sm lg:before:hover:opacity-100"
+    >
+        <div
+            class="h-full rounded-[10px] bg-blue-300 p-4 !pt-10 dark:bg-slate-900 sm:p-6"
+        >
+            <time
+                :datetime="post.date.toDateString()"
+                class="text-xs text-gray-500 dark:text-gray-400"
+            >
+                {{ post.date.toDateString() }}
+            </time>
+
+            <a href="#">
+                <h3
+                    class="mt-0.5 text-lg font-medium text-gray-900 dark:text-white"
+                >
+                    {{ post.title }}
+                </h3>
+            </a>
+
+            <div class="flex items-end justify-between">
+                <div class="mt-4 flex flex-wrap gap-1">
+                    <span
+                        v-for="(tag, index) in post.tags"
+                        :key="index"
+                        class="whitespace-nowrap rounded-full bg-themeGreen px-2.5 py-0.5 text-xs text-slate-600 dark:bg-purple-600 dark:text-purple-100"
+                    >
+                        {{ tag }}
+                    </span>
+                </div>
+                <Icon
+                    name="heroicons-outline:arrow-narrow-right"
+                    size="2em"
+                    class="flex-shrink-0 text-themePurple lg:opacity-0 lg:transition lg:duration-300 lg:ease-linear lg:group-hover:scale-125 lg:group-hover:opacity-100"
+                />
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/UI/Section.vue
@@ -0,0 +1,12 @@
+<template>
+    <div
+        v-motion-fade-visible-once
+        class="relative flex flex-col gap-10 lg:after:absolute lg:after:bottom-0 lg:after:right-0 lg:after:h-48 lg:after:w-96 lg:after:bg-[radial-gradient(#7354F2,transparent_2px)] lg:after:[background-size:25px_25px] dark:lg:after:bg-[radial-gradient(#47F75F,transparent_2px)]"
+    >
+        <div
+            class="prose prose-lg flex flex-col space-y-2 dark:prose-invert prose-h2:mb-2 prose-h2:mt-10"
+        >
+            <slot></slot>
+        </div>
+    </div>
+</template>
